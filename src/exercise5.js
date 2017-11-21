638d7551347ed87d553115b6b10c6974b7bb45ac
pierre chene <pierre.chene44@gmail.com>
2017-11-21 17:02:12 +0100

    Ajout de l'exercice 5 qui a passé le test

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,18 @@
+var fs=require('fs');
+
 exports.run = function(input) {
   console.log("Exercise #5");
+  var total=0;
+
+  fs.readdir(input.directory, function (err, files) {
+    if(err)
+      return input.showError;
+    files.forEach(element=>{
+      total++;
+    });
+    
+    return input.showDone(total);
+  
+  });
+
 };
