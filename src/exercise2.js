3b286ad01451ba21986e92f249c630a2c99c00d9
pierre chene <pierre.chene44@gmail.com>
2017-11-21 16:11:45 +0100

    fin de l'exercice 2 qui a passé le test

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,15 @@
 exports.run = function(input) {
   console.log("Exercise #2");
+
+  string=input.split('');
+  var bigger=0;
+  string.forEach(element => {
+    element=parseInt(element);
+    if(element>bigger)
+      bigger=element;
+  });
+
+  return bigger;
 };
+
+
