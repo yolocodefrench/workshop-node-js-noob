eeb6f002e8bd0f71c7d068f85ef9010ce728a8b4
pierre chene <pierre.chene44@gmail.com>
2017-11-21 16:43:54 +0100

    ajout de l'exercice 4 qui a passé les tests

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,7 +2,15 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+  function repeat (repetitions,string)
+  {
+      var chaineFinale=string;
 
+      for(var j=1;j<repetitions;j++)
+        chaineFinale+=`, ${string}`;
+
+      return chaineFinale;
+  }
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
   /** */
