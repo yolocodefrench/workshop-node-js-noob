1a8cd444947d3756035c5135bb97194c82745baa
pierre chene <pierre.chene44@gmail.com>
2017-11-21 16:20:10 +0100

    ajout de l'exercice 3 qui a passé les tests

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,20 @@
 exports.run = function(input) {
   console.log("Exercise #3");
+
+  var array=[];
+  var total=0;
+
+  input.forEach(element => {//si multiply est true, alors on ajoute la valeur au tableau
+    if(element.multiply===true)
+      array.push(element.value);
+  });
+
+  total = array[0];//le total doit etre initialisé, sinon, la multiplication rendra toujours 0
+
+  for(var i=1;i<array.length;i++){
+    total*=array[i];
+  }
+
+  return total;
+
 };
